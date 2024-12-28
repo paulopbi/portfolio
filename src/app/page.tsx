0223808a097f4900fadf11ee20b5a0159401ebd2
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-28 10:44:23 -0300

    feat: add all project sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Estudos from "@/sections/Estudos";
+import Habilidades from "@/sections/Habilidades";
 import Hero from "@/sections/Hero";
+import Projetos from "@/sections/Projetos";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Habilidades />
+      <Projetos />
+      <Estudos />
+    </>
+  );
 }
