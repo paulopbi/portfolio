b0732d0a2496dc1bc5e57e51b023281b8b5d5ce6
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-30 09:12:51 -0300

    feat: add estudos section

--- src/sections/Estudos.tsx
+++ src/sections/Estudos.tsx
@@ -1,3 +1,6 @@
+import Ornament_Icon from "@/components/Icons/Ornament_Icon";
+import SectionHeading from "@/components/SectionHeading";
+
 const formacoes = [
   {
     id: 1,
@@ -59,33 +62,39 @@ const formacoes = [
 
 export default function Estudos() {
   return (
-    <section className="container mx-auto w-full px-4 py-10">
-      <div className="text-center">
-        <h3 className="section-title title-gradient">Estudos</h3>
-        <span className="section-subtitle">
-          Demonstrações e repositórios dos meus estudos.
-        </span>
+    <section
+      id="educacao"
+      className="container relative mx-auto w-full px-4 py-10"
+    >
+      <div className="absolute left-0 top-[70px]">
+        <Ornament_Icon />
       </div>
+      <div className="absolute bottom-[70px] right-0">
+        <Ornament_Icon />
+      </div>
+      <SectionHeading title="Educação">
+        Meus certificações, diplomas e cursos de T.I ou programação
+      </SectionHeading>
 
-      <section className="container mx-auto grid grid-cols-1 gap-4 py-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <article className="container mx-auto grid grid-cols-1 gap-4 py-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {formacoes.map((formacao) => (
           <div
             key={formacao.id}
-            className="flex flex-col justify-between rounded-md border border-white bg-indigo-900 p-4 md:min-h-[230px] md:max-w-[400px]"
+            className="flex flex-col justify-between rounded-md border border-indigo-400 bg-indigo-950 p-4 md:min-h-[230px] md:max-w-[400px]"
           >
             <div className="pb-8">
               <h4 className="mb-2 text-base font-medium">{formacao.grau}</h4>
-              <h3 className="max-w-[35ch] text-2xl font-bold leading-8 tracking-normal">
+              <h3 className="max-w-[300px] text-2xl font-bold capitalize leading-8 tracking-normal">
                 {formacao.nome}
               </h3>
             </div>
-            <div className="flex items-center justify-between text-sm font-light uppercase">
+            <div className="flex items-center justify-between text-sm font-normal uppercase">
               <span>{formacao.instituicao}</span>
               <span>{formacao.horas}H</span>
             </div>
           </div>
         ))}
-      </section>
+      </article>
     </section>
   );
 }
