9657860b567fb99d52009193761fd3c0fb38aa10
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-12 07:40:13 -0300

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { DiGithubBadge } from "react-icons/di"
+import { LiaLinkedinIn } from "react-icons/lia"
+import { MdAlternateEmail } from "react-icons/md"
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <footer className="container mt-16 flex flex-col items-center justify-between gap-4 py-2 md:flex-row">
+      <p className="text-muted-opacity text-sm font-normal">
+        &copy; {currentYear} Paulo Victor Silva. Todos os direitos reservados.
+      </p>
+      <div className="inline-flex gap-2">
+        <DiGithubBadge size={24} />
+        <LiaLinkedinIn size={24} />
+        <MdAlternateEmail size={24} />
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
