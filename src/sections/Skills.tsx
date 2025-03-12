cc3d14f30cef9b3c004fecf824cd6243a1ab70bc
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-12 08:04:07 -0300

    feat: add marquee effect

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,5 +1,4 @@
-import React from "react"
-
+"use client"
 import ReactIcon from "@/components/Icons/React_Icon"
 import JavascriptIcon from "@/components/Icons/JS_Icon"
 import FigmaIcon from "@/components/Icons/Figma_Icon"
@@ -14,6 +13,8 @@ import NextIcon from "@/components/Icons/Next_Icon"
 import NodeIcon from "@/components/Icons/Node_Icon"
 import PostgresIcon from "@/components/Icons/Postgres_Icon"
 import PrismaIcon from "@/components/Icons/Prisma_Icon"
+
+import { motion } from "framer-motion"
 
 const stacks = [
   {
@@ -76,16 +77,32 @@ const stacks = [
 
 const Skills = () => {
   return (
-    <section className="my-16 flex w-full items-center justify-center gap-8 overflow-hidden border-t border-b border-white/30 py-8 whitespace-nowrap">
-      {stacks.map((stack) => (
-        <div
-          key={stack.name}
-          className="bg-foreground inline-flex min-w-fit items-center justify-center gap-2 rounded-full px-5 py-2"
-        >
-          <span className="text-sm text-nowrap">{stack.name}</span>
-          <stack.icon />
-        </div>
-      ))}
+    <section className="mt-16 w-full overflow-hidden border-t border-b border-white/30 py-8 whitespace-nowrap">
+      <motion.div
+        initial={{ translateX: 0 }}
+        animate={{ translateX: "-100%" }}
+        transition={{ duration: "50", ease: "linear", repeat: Infinity }}
+        className="inline-flex gap-6"
+      >
+        {stacks.map((stack) => (
+          <div
+            key={stack.name}
+            className="bg-foreground inline-flex min-w-fit items-center justify-center gap-2 rounded-full px-5 py-2"
+          >
+            <span className="text-sm text-nowrap">{stack.name}</span>
+            <stack.icon />
+          </div>
+        ))}
+        {stacks.map((stack) => (
+          <div
+            key={stack.name}
+            className="bg-foreground inline-flex min-w-fit items-center justify-center gap-2 rounded-full px-5 py-2"
+          >
+            <span className="text-sm text-nowrap">{stack.name}</span>
+            <stack.icon />
+          </div>
+        ))}
+      </motion.div>
     </section>
   )
 }
