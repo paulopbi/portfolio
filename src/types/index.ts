8aa783a7dc4e0f5ff9ec96f295221547c4c79d41
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-13 10:26:43 -0300

    feat: add ICardProjects interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,17 @@
+import { StaticImageData } from "next/image"
+
 export interface ICardInfo {
   image?: string
+  href?: string
   icon?: React.ReactNode
   jobTitle: string
   companyName: string
   whenStart: string
   whenFinish: string
+}
+
+export interface ICardProject {
+  imageSrc: StaticImageData | string
+  title: string
+  skills: string[]
 }
