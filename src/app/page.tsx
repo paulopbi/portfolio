92830af2719771e0b8aca6e5dee85c6fe885cc6b
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-16 18:15:00 -0300

    feat: add homepage sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+"use client"
+import Hero from "@/sections/Hero"
+import Skills from "@/sections/Skills"
+import Education from "@/sections/Education"
+import Projects from "@/sections/Projects"
+import Experience from "@/sections/Experience"
+import About from "@/sections/About"
+
 export default function Home() {
-    return <div className="text-underline text-3xl">Hello World</div>
+  return (
+    <main>
+      <Hero />
+      <Experience />
+      <Education />
+      <About />
+      <Skills />
+      <Projects />
+    </main>
+  )
 }
