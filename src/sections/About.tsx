55912fcb1260b82af6f9d6e83cdb1a6b5a26947d
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-16 18:14:35 -0300

    feat: add about component

---
+++ src/sections/About.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { TextReveal } from "@/components/magicui/text-reveal"
+import React from "react"
+
+const About = () => {
+  return (
+    <div className="my-10">
+      <TextReveal className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+        quaerat sequi. Repellendus pariatur odit aut praesentium perspiciatis
+        rem cum ea reprehenderit iste, vitae dolorem quod architecto minima sed
+        vel quam.
+      </TextReveal>
+    </div>
+  )
+}
+
+export default About
