95c58031e0c4727cb35ffed6cdefcb80147f5692
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-16 18:11:51 -0300

    feat: add navbar scroll-progress component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,38 +3,44 @@ import Image from "next/image"
 import USAFlag from "@/assets/images/usa_flag.png"
 import { motion } from "framer-motion"
 import { IoMenu } from "react-icons/io5"
+import { ScrollProgress } from "./magicui/scroll-progress"
 
 const Navbar = () => {
   return (
-    <motion.header
-      className="sticky top-0 left-0 z-10 w-full bg-transparent p-4 backdrop-blur-xl"
-      initial={{ y: -40, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-    >
-      <nav className="container flex items-center justify-between">
-        <div className="font-regular text-brand text-2xl">Paulo Victor</div>
+    <>
+      <motion.header
+        className="sticky top-0 left-0 z-99 w-full p-4 backdrop-blur-2xl"
+        initial={{ y: -40, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <div className="container flex items-center justify-between">
+          <div className="text-brand text-md font-medium">Logo</div>
 
-        <nav>
-          <ul className="text-muted flex items-center gap-4 font-light">
-            <li>Sobre</li>
-            <li>Projetos</li>
-            <li>Educação</li>
-          </ul>
-        </nav>
+          <nav>
+            <ul className="text-muted flex items-center gap-4 font-light">
+              <li>Sobre</li>
+              <li>Projetos</li>
+              <li>Educação</li>
+            </ul>
+          </nav>
 
-        <div className="flex items-center gap-4">
-          <div>
-            <Image
-              src={USAFlag}
-              width={32}
-              height={32}
-              alt="English language"
-            />
+          <div className="flex items-center gap-4">
+            <div>
+              <Image
+                src={USAFlag}
+                width={32}
+                height={32}
+                alt="English language"
+              />
+            </div>
+            <IoMenu size={24} />
           </div>
-          <IoMenu size={24} />
         </div>
-      </nav>
-    </motion.header>
+      </motion.header>
+      <div className="z-10 rounded-lg p-4">
+        <ScrollProgress className="top-[65px]" />
+      </div>
+    </>
   )
 }
 
