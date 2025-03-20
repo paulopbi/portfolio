d0422c3b97035b9e3434b5dac8ca0678604baf59
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-20 10:08:43 -0300

    refactor: add new animations and vertical marquee effect

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,4 +1,5 @@
 "use client"
+import React from "react"
 import ReactIcon from "@/components/Icons/React_Icon"
 import JavascriptIcon from "@/components/Icons/JS_Icon"
 import FigmaIcon from "@/components/Icons/Figma_Icon"
@@ -14,6 +15,7 @@ import NodeIcon from "@/components/Icons/Node_Icon"
 import PostgresIcon from "@/components/Icons/Postgres_Icon"
 import PrismaIcon from "@/components/Icons/Prisma_Icon"
 import { motion } from "framer-motion"
+import { TextAnimate } from "@/components/magicui/text-animate"
 
 const stacks = [
   {
@@ -74,34 +76,78 @@ const stacks = [
   },
 ]
 
-const Skills = () => {
+function SkillColumn(props: {
+  stacks: typeof stacks
+  className?: string
+  duration?: number
+}) {
   return (
-    <section className="mt-16 w-full overflow-hidden border-t border-b border-white/10 py-8 whitespace-nowrap">
+    <div className={props.className}>
       <motion.div
-        initial={{ translateX: 0 }}
-        animate={{ translateX: "-100%" }}
-        transition={{ duration: "50", ease: "linear", repeat: Infinity }}
-        className="inline-flex gap-6"
+        animate={{ translateY: "-50%" }}
+        transition={{
+          duration: props.duration || 10,
+          repeat: Infinity,
+          ease: "linear",
+          repeatType: "loop",
+        }}
+        className="flex flex-col gap-6 pb-6"
       >
-        {stacks.map((stack) => (
-          <div
-            key={stack.name}
-            className="bg-foreground inline-flex min-w-fit items-center justify-center gap-2 rounded-full px-5 py-2"
-          >
-            <span className="text-sm text-nowrap">{stack.name}</span>
-            <stack.icon />
-          </div>
-        ))}
-        {stacks.map((stack) => (
-          <div
-            key={stack.name}
-            className="bg-foreground inline-flex min-w-fit items-center justify-center gap-2 rounded-full px-5 py-2"
-          >
-            <span className="text-sm text-nowrap">{stack.name}</span>
-            <stack.icon />
-          </div>
-        ))}
+        {[
+          ...new Array(2).fill(0).map((_, index) => (
+            <React.Fragment key={index}>
+              {props.stacks.map((stack) => (
+                <div
+                  key={stack.name}
+                  className="bg-foreground border-brand flex min-h-[140px] w-[150px] flex-col items-center justify-center gap-5 rounded-3xl border-2 px-4 py-6"
+                >
+                  <h5 className="text-center text-sm font-bold">
+                    {stack.name}
+                  </h5>
+                  <stack.icon width={36} height={36} />
+                </div>
+              ))}
+            </React.Fragment>
+          )),
+        ]}
       </motion.div>
+    </div>
+  )
+}
+
+const Skills = () => {
+  const firstColumn = stacks
+  const secoundColumn = stacks.reverse()
+
+  return (
+    <section className="container mt-20 grid grid-cols-1 items-center md:grid-cols-2">
+      <div>
+        <TextAnimate
+          className="subtitle"
+          by="character"
+          as={"h3"}
+          animation="scaleDown"
+        >
+          Habilidades
+        </TextAnimate>
+        <p className="subtitle__description">
+          Cada quadrado desse representa uma etapa da minha jornada de
+          aprendizado e prática, juntas, essas tecnologias formam o conjunto de
+          ferramentas que uso para criar experiências digitais impactantes.
+        </p>
+      </div>
+
+      {/* vertical marquee effect */}
+      <div className="relative container mx-auto">
+        <div className="mt-10 flex max-h-[738px] justify-center gap-6 overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
+          <SkillColumn stacks={firstColumn} duration={40} />
+          <SkillColumn
+            stacks={secoundColumn}
+            duration={60}
+            className="hidden md:block"
+          />
+        </div>
+      </div>
     </section>
   )
 }
