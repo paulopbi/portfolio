cee2e23ac1aa1bd4c9a64f7ca5c7a6b29c4b3e0e
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-20 10:20:19 -0300

    refactor: update the section with new layout

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -4,25 +4,42 @@ import { GraduationCap } from "lucide-react"
 
 const Education = () => {
   return (
-    <section className="relative container mt-16" id="education">
-      <TextAnimate className="text-muted text-center text-sm font-light">
-        Estudos
-      </TextAnimate>
+    <section
+      className="container my-25 grid grid-cols-1 items-start gap-10 md:grid-cols-2"
+      id="education"
+    >
+      <div>
+        <TextAnimate
+          className="subtitle"
+          by="character"
+          as={"h3"}
+          animation="scaleDown"
+        >
+          Estudos e Especialização
+        </TextAnimate>
+        <p className="subtitle__description">
+          Para mim, estudo e trabalho andam juntos. Cada nova aprendizagem é um
+          passo rumo à excelência no que faço.
+        </p>
+      </div>
 
-      <CardInfo
-        icon={<GraduationCap />}
-        jobTitle="análise e desenvolvimento de sistemas"
-        companyName="unicesumar"
-        whenStart="2022"
-        whenFinish="2024"
-      />
-      <CardInfo
-        icon={<GraduationCap />}
-        jobTitle="técnico em eletrotécnica"
-        companyName="escola técnica sandra silva"
-        whenStart="2019"
-        whenFinish="2024"
-      />
+      {/* card info with educational things */}
+      <div>
+        <CardInfo
+          icon={<GraduationCap />}
+          jobTitle="análise e desenvolvimento de sistemas"
+          companyName="unicesumar"
+          whenStart="2022"
+          whenFinish="2024"
+        />
+        <CardInfo
+          icon={<GraduationCap />}
+          jobTitle="técnico em eletrotécnica"
+          companyName="escola técnica sandra silva"
+          whenStart="2019"
+          whenFinish="2024"
+        />
+      </div>
     </section>
   )
 }
