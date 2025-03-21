e348d7c49593256612bd74e963062a783db10a13
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-21 07:51:53 -0300

    chore: add stacks constant value

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
+/* demo */
 import Dogs from "../assets/demo/dogs.png"
 import LemonPeper from "../assets/demo/lemon_peper.jpg"
 import MovieMate from "../assets/demo/movie_mate.jpeg"
@@ -5,6 +6,81 @@ import CodeLandia from "../assets/demo/codelandia.jpg"
 import FlashDash from "../assets/demo/flashdash.gif"
 import Sass from "../assets/demo/sass.gif"
 import RickAndMorty from "../assets/demo/rick_morty.gif"
+
+/* icons */
+import ReactIcon from "@/components/Icons/React_Icon"
+import JavascriptIcon from "@/components/Icons/JS_Icon"
+import FigmaIcon from "@/components/Icons/Figma_Icon"
+import TailwindIcon from "@/components/Icons/Tailwind_Icon"
+import TypeIcon from "@/components/Icons/Type_Icon"
+import SassIcon from "@/components/Icons/Sass_Icon"
+import StyledComponentsIcon from "@/components/Icons/Styled_Icon"
+import GitIcon from "@/components/Icons/Git_Icon"
+import HTMLIcon from "@/components/Icons/HTML_Icon"
+import MongoDBIcon from "@/components/Icons/Mongo_Icon"
+import NextIcon from "@/components/Icons/Next_Icon"
+import NodeIcon from "@/components/Icons/Node_Icon"
+import PostgresIcon from "@/components/Icons/Postgres_Icon"
+import PrismaIcon from "@/components/Icons/Prisma_Icon"
+
+export const stacks = [
+  {
+    name: "React",
+    icon: ReactIcon,
+  },
+  {
+    name: "Typescript",
+    icon: TypeIcon,
+  },
+  {
+    name: "Javascript",
+    icon: JavascriptIcon,
+  },
+  {
+    name: "Figma",
+    icon: FigmaIcon,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: TailwindIcon,
+  },
+  {
+    name: "SASS",
+    icon: SassIcon,
+  },
+  {
+    name: "GIT",
+    icon: GitIcon,
+  },
+  {
+    name: "Styled Components",
+    icon: StyledComponentsIcon,
+  },
+  {
+    name: "Node JS",
+    icon: NodeIcon,
+  },
+  {
+    name: "Next JS",
+    icon: NextIcon,
+  },
+  {
+    name: "Postgres SQL",
+    icon: PostgresIcon,
+  },
+  {
+    name: "Prisma",
+    icon: PrismaIcon,
+  },
+  {
+    name: "HTML",
+    icon: HTMLIcon,
+  },
+  {
+    name: "Mongo DB",
+    icon: MongoDBIcon,
+  },
+]
 
 export const projectsInfo = [
   {
