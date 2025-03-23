2bc85747536a68fbc08bac93637a519fc8d862d6
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-23 20:25:02 -0300

    feat: add Bricolage Grotesque font

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,6 +1,11 @@
-import { Geist } from "next/font/google"
+import { Bricolage_Grotesque, Geist } from "next/font/google"
 
 export const geistSansFont = Geist({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+})
+
+export const BricolageGrotesqueFont = Bricolage_Grotesque({
+  weight: ["700"],
   subsets: ["latin"],
 })
