09fbd9a8ef748b7b2dce2139b44fece2c85bbc76
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-23 20:31:44 -0300

    feat: create projects page

---
+++ src/app/projetos/page.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import CardProject from "@/components/card-project"
+import { projectsContentInfo } from "@/constants/project/index"
+import { TextAnimate } from "@/components/magicui/text-animate"
+
+const ProjectsPage = () => {
+  return (
+    <section className="relative container mt-10 mb-20">
+      <TextAnimate className="subtitle--heading text-center">
+        Todos os Projetos
+      </TextAnimate>
+
+      {/* card content */}
+      <div className="mx-auto mt-16 grid min-h-dvh grid-cols-1 place-content-center gap-8 gap-x-20 gap-y-10 md:grid-cols-2">
+        {projectsContentInfo.map((project) => (
+          <CardProject
+            key={project.slug}
+            title={project.title}
+            hrefTo={`/projetos/${project.slug}`}
+            imageSrc={project.image_url}
+            skills={project.tags}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsPage
