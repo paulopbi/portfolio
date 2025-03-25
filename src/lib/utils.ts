be335a240300ab1b3ab71d6b7fbe185e1ac73d73
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-25 07:41:51 -0300

    refactor: simplify the logic

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,8 +9,8 @@ export const highlightTags = (tags: string) => {
   const shouldHighlight =
     tags.includes("next") ||
     tags.includes("react") ||
-    tags.includes("UI Design")
-  return shouldHighlight
-    ? "inline-block border border-white/40 rounded-full px-2.5 py-1.5 text-sm leading-none font-bold tracking-wide bg-[linear-gradient(to_right,#dd7ddf,#e1cd86,#bbcb92,#71c2ef,#3bffff,#dd7ddf,#e1cd86,#bbcb92,#71c2ef,#3bffff,#dd7ddf)] bg-clip-text text-transparent [background-size:200%]"
-    : "inline-block border border-white/40 rounded-full px-2.5 py-1.5 text-sm text-muted leading-none font-medium tracking-wide"
+    tags.includes("ui design")
+  const textHighlightClassName =
+    "inline-block rounded-full border border-white/40 bg-[linear-gradient(to_right,#dd7ddf,#e1cd86,#bbcb92,#71c2ef,#3bffff,#dd7ddf,#e1cd86,#bbcb92,#71c2ef,#3bffff,#dd7ddf)] [background-size:200%] bg-clip-text px-2.5 py-1.5 text-sm leading-none font-bold tracking-wide text-transparent capitalize"
+  return shouldHighlight ? textHighlightClassName : "badge-style"
 }
