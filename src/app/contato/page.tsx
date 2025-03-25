3d5af2b6a77a0c690db7edfa9388d5b7991fcc25
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-25 09:50:47 -0300

    feat: add contact page

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,9 @@
+const ContactPage = () => {
+  return (
+    <section className="relative z-10 container grid min-h-dvh grid-cols-2 items-center justify-center">
+      <h1>contato</h1>
+    </section>
+  )
+}
+
+export default ContactPage
