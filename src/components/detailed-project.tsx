d2bd66f511cf16ef901d5801bf9095028e1fd5c3
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-25 09:10:12 -0300

    refactor: improves structure

--- src/components/detailed-project.tsx
+++ src/components/detailed-project.tsx
@@ -13,7 +13,7 @@ import { highlightTags } from "@/lib/utils"
 const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
   return (
     <section className="relative z-10 container">
-      {/* header */}
+      {/* return link navegation */}
       <div className="flex items-center justify-between gap-4">
         <Link
           className="text-muted inline-flex items-center justify-between gap-2 text-base font-light"
@@ -22,7 +22,7 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
           <ArrowLeft size={18} color="#e5e5e5" /> Projetos
         </Link>
 
-        <Badge className="text-muted rounded-md border-white/15 bg-transparent px-2 py-1 text-sm">
+        <Badge className="text-muted rounded-full border border-white/20 bg-transparent px-2 py-1">
           {data.map(({ year }) => year)}
         </Badge>
       </div>
@@ -31,22 +31,38 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
       <article className="mb-10">
         {data.map((project) => (
           <React.Fragment key={project.slug}>
+            {/* cover picture */}
+            <section>
+              {project.cover && (
+                <div className="mt-10 overflow-hidden rounded-3xl">
+                  <Image
+                    src={project.cover}
+                    alt={`Banner do projeto ${project.title}`}
+                    width={1000}
+                    height={300}
+                    fetchPriority="high"
+                    className="block h-full w-full object-cover object-center"
+                  />
+                </div>
+              )}
+            </section>
+
             {/* title with button links */}
-            <section className="mt-10 flex w-full flex-col flex-wrap items-center justify-center gap-4">
-              <TextAnimate className="title text-white not-italic" as={"h2"}>
+            <section className="mt-10 flex w-full flex-col flex-wrap items-center justify-center gap-4 md:flex-row md:justify-between">
+              <TextAnimate className="title not-italic" as={"h2"}>
                 {project.title}
               </TextAnimate>
 
               <div className="flex gap-x-5">
                 {project.demo_url && (
-                  <Button asChild>
+                  <Button asChild variant="outline">
                     <a href={project.demo_url} target="_blank" rel="no-refer">
                       Demonstração <ArrowUpRightIcon />
                     </a>
                   </Button>
                 )}
                 {project.repository && (
-                  <Button asChild>
+                  <Button asChild variant="outline">
                     <a href={project.repository} target="_blank">
                       Repositório <ArrowUpRightIcon />
                     </a>
@@ -55,8 +71,8 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
               </div>
             </section>
 
-            {/* badge tags */}
-            <section className="mt-5 inline-flex w-full flex-wrap items-center justify-center gap-4">
+            {/* hightlight tags badge */}
+            <section className="mt-5 inline-flex w-full flex-wrap items-center justify-center gap-4 border-b border-white/10 pb-5 md:items-start md:justify-start">
               {project.tags.map((tag) => (
                 <motion.div
                   animate={{ backgroundPositionX: "-100%" }}
@@ -74,50 +90,24 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
               ))}
             </section>
 
-            {/* cover picture */}
-            <section>
-              {project.cover && (
-                <div className="mt-10 overflow-hidden rounded-3xl">
-                  <Image
-                    src={project.cover}
-                    alt={`Banner do projeto ${project.title}`}
-                    width={1000}
-                    height={300}
-                    fetchPriority="high"
-                    className="block h-full w-full object-contain object-center"
-                  />
-                </div>
-              )}
-            </section>
-
             {/* overview */}
-            <section className="mt-8 flex flex-col">
-              <h6 className="subtitle border-b border-white/15 pb-2">
-                Descrição
-              </h6>
-              <p
-                className="description mt-4"
-                dangerouslySetInnerHTML={{ __html: project.description }}
-              />
-            </section>
+            <p
+              className="description mx-auto mt-10 max-w-[900px] text-justify text-pretty"
+              dangerouslySetInnerHTML={{ __html: project.description }}
+            />
 
             {/* image preview */}
             {project.image_url && (
-              <section className="subtitle mt-8">
-                <h6 className="subtitle border-b border-white/15 pb-2">
-                  Prévia
-                </h6>
-                <div className="mt-3 max-h-[800px] min-w-full overflow-hidden rounded-3xl">
-                  <Image
-                    unoptimized
-                    loading="lazy"
-                    src={project.image_url}
-                    width={1920}
-                    height={1080}
-                    alt={`foto de capa do projeto ${project.title}`}
-                    className="block h-full w-full object-cover object-center"
-                  />
-                </div>
+              <section className="mx-auto mt-10 max-h-[900px] w-[900px] overflow-hidden rounded-2xl">
+                <Image
+                  unoptimized
+                  loading="lazy"
+                  src={project.image_url}
+                  width={900}
+                  height={900}
+                  alt={`foto de capa do projeto ${project.title}`}
+                  className="block h-full w-full object-cover object-center"
+                />
               </section>
             )}
           </React.Fragment>
