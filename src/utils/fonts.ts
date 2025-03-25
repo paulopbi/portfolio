5de5266d8c8f981b09bde14e26d38f7a9d1b13ff
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-25 11:07:42 -0300

    fix: add font style

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -2,10 +2,12 @@ import { Bricolage_Grotesque, Inter } from "next/font/google"
 
 export const interSansFont = Inter({
   weight: ["400", "500", "600"],
+  style: "normal",
   subsets: ["latin"],
 })
 
 export const BricolageGrotesqueFont = Bricolage_Grotesque({
   weight: ["700"],
+  style: "normal",
   subsets: ["latin"],
 })
