2641b4120d1971d18c78f9960e515d542eda5717
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-26 11:28:07 -0300

    feat: add footer component to projects page

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -2,27 +2,31 @@ import React from "react"
 import CardProject from "@/components/card-project"
 import { projectInfo } from "@/constants/projectConstants"
 import { TextAnimate } from "@/components/magicui/text-animate"
+import Footer from "@/components/footer"
 
 const ProjectsPage = () => {
   return (
-    <section className="relative container mt-10 mb-20">
-      <TextAnimate className="subtitle--heading text-center">
-        Todos os Projetos
-      </TextAnimate>
+    <>
+      <section className="relative container mt-10 mb-20">
+        <TextAnimate className="subtitle--heading text-center">
+          Todos os Projetos
+        </TextAnimate>
 
-      {/* card content */}
-      <div className="mx-auto mt-16 grid min-h-dvh grid-cols-1 place-content-center gap-8 gap-x-20 gap-y-10 md:grid-cols-2">
-        {projectInfo.map((project) => (
-          <CardProject
-            key={project.slug}
-            title={project.title}
-            hrefTo={`/projetos/${project.slug}`}
-            imageSrc={project.image_url}
-            skills={project.tags}
-          />
-        ))}
-      </div>
-    </section>
+        {/* card content */}
+        <div className="mx-auto mt-16 grid min-h-dvh grid-cols-1 place-content-center gap-8 gap-x-20 gap-y-10 md:grid-cols-2">
+          {projectInfo.map((project) => (
+            <CardProject
+              key={project.slug}
+              title={project.title}
+              hrefTo={`/projetos/${project.slug}`}
+              imageSrc={project.image_url}
+              skills={project.tags}
+            />
+          ))}
+        </div>
+      </section>
+      <Footer />
+    </>
   )
 }
 
