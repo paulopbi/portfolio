7d518a3949c076540725a24c004c1f34544e5cc6
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-26 11:28:45 -0300

    fix: add metadata to layout

--- src/app/contato/layout.tsx
+++ src/app/contato/layout.tsx
@@ -1,3 +1,18 @@
+import type { Metadata } from "next"
+import { SeoKeyWords } from "@/constants"
+
+export const metadata: Metadata = {
+  title: "Paulo Victor - Contato",
+  description: "modificar",
+  authors: {
+    name: "Paulo Victor Silva Rosa",
+  },
+  icons: {
+    icon: "./src/app/favicon.ico",
+  },
+  keywords: SeoKeyWords,
+}
+
 export default function ContactLayout({
   children,
 }: Readonly<{
