9252845cb8d3df72d3d79d60b51d58978dd907ac
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-26 10:31:35 -0300

    feat: create contact route

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,7 +1,39 @@
+import Image from "next/image"
+import ContactPicture from "@/assets/images/picture_contact.jpg"
+
+import { contactsPageLinks } from "@/constants/contactConstants"
+
 const ContactPage = () => {
   return (
-    <section className="relative z-10 container grid min-h-dvh grid-cols-2 items-center justify-center">
-      <h1>contato</h1>
+    <section className="relative z-10 container flex min-h-dvh flex-col items-center justify-center">
+      {/* image */}
+      <div className="mx-auto mb-2 size-52 overflow-hidden rounded-full border-3 border-white/50">
+        <Image
+          src={ContactPicture}
+          alt="Foto do programador Paulo Victor no cristo redentor Rio de Janeiro"
+          width={208}
+          height={208}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+
+      {/* name */}
+      <p className="subtitle mb-10 text-center">Paulo Victor Silva</p>
+
+      {/* links */}
+      <div className="mb-10 flex w-full max-w-[540px] flex-col items-center justify-center gap-4">
+        {contactsPageLinks.map((link) => (
+          <a
+            key={link.name}
+            href={link.href}
+            target="_blank"
+            rel="noreferrer"
+            className="bg-foreground inline-block w-full rounded-[8px] border border-white/50 px-6 py-4 text-center text-base font-medium capitalize"
+          >
+            {link.name}
+          </a>
+        ))}
+      </div>
     </section>
   )
 }
---
+++ src/constants/contactConstants.ts
@@ -0,0 +1,7 @@
+export const contactsPageLinks = [
+  { href: "https://github.com/paulopbi", name: "github" },
+  { href: "https://www.linkedin.com/in/paulopbi/", name: "linkedin" },
+  { href: "https://x.com/UflyDev", name: "X" },
+  { href: "https://www.instagram.com/paulopbi_/", name: "instagram" },
+  { href: "mailto:paulovictordev@gmail.com", name: "gmail" },
+]
