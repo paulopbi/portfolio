27021240161f60aa15cb92172212ea889db5d051
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-27 08:03:43 -0300

    feat: add contact page

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image"
+import React from "react"
+import ContactPicture from "@/assets/images/picture_contact.jpg"
+
+import { contactsPageLinks } from "@/constants/contactConstants"
+import { Button } from "@/components/ui/button"
+import { Particles } from "@/components/magicui/particles"
+const Contact = () => {
+  return (
+    <section className="relative z-10 container flex min-h-dvh flex-col items-center justify-center gap-2">
+      <Particles
+        className="absolute inset-0 z-0 h-full w-full"
+        quantity={290}
+        ease={120}
+        color="#ccc"
+        refresh
+      />
+
+      {/* image */}
+      <div className="mx-auto mb-2 size-52 overflow-hidden rounded-full border-3 border-white/50 duration-75 ease-in hover:scale-[1.1]">
+        <Image
+          src={ContactPicture}
+          alt="Foto do programador Paulo Victor no cristo redentor Rio de Janeiro"
+          width={208}
+          height={208}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+
+      {/* texts */}
+      <div className="mb-10 inline-flex flex-col items-center justify-center gap-1">
+        <h6 className="subtitle text-brand">Paulo Victor Silva</h6>
+        <span className="description">Me siga nas redes sociais abaixo 👇🏾</span>
+      </div>
+
+      {/* links */}
+
+      <div className="mb-10 flex w-full max-w-[540px] flex-col items-center justify-center gap-4">
+        {contactsPageLinks.map((link) => (
+          <Button
+            asChild
+            key={link.name}
+            variant="outline"
+            className="min-h-[40px] w-full uppercase"
+          >
+            <a href={link.href} target="_blank" rel="noreferrer">
+              {link.name}
+            </a>
+          </Button>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Contact
