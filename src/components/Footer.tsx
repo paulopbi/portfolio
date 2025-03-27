0e1659463e31ab379a2058e50181355942e5ae42
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-27 09:36:17 -0300

    fix: add social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,15 @@ const Footer = () => {
         &copy; {currentYear} Paulo Victor Silva. Todos os direitos reservados.
       </p>
       <div className="inline-flex gap-2">
-        <Github size={24} />
-        <Linkedin size={24} />
-        <Mail size={24} />
+        <a href="https://github.com/paulopbi" target="_blank" rel="noreferrer">
+          <Github size={24} />
+        </a>
+        <a href="https://www.linkedin.com/in/paulopbi/">
+          <Linkedin size={24} />
+        </a>
+        <a href="mailto:paulovictordev@gmail.com">
+          <Mail size={24} />
+        </a>
       </div>
     </footer>
   )
