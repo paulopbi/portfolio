d0b65de28d79dcc08fd98afc3650cc0a4ae24f78
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-27 09:04:49 -0300

    refactor: remove banner feature

--- src/components/detailed-project.tsx
+++ src/components/detailed-project.tsx
@@ -13,8 +13,8 @@ import { highlightTags } from "@/lib/utils"
 const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
   return (
     <section className="relative z-10 container">
-      {/* return link navegation */}
-      <div className="flex items-center justify-between gap-4">
+      {/* navegation on top */}
+      <section className="flex items-center justify-between gap-4">
         <Link
           className="text-muted inline-flex items-center justify-between gap-2 text-base font-light"
           href="/projetos"
@@ -25,54 +25,21 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
         <Badge className="text-muted rounded-full border border-white/20 bg-transparent px-2 py-1">
           {data.map(({ year }) => year)}
         </Badge>
-      </div>
+      </section>
 
-      {/* main content */}
-      <article className="mb-10">
-        {data.map((project) => (
-          <React.Fragment key={project.slug}>
-            {/* cover picture */}
-            <section>
-              {project.cover && (
-                <div className="mt-10 overflow-hidden rounded-3xl">
-                  <Image
-                    src={project.cover}
-                    alt={`Banner do projeto ${project.title}`}
-                    width={1000}
-                    height={300}
-                    fetchPriority="high"
-                    className="block h-full w-full object-cover object-center"
-                  />
-                </div>
-              )}
-            </section>
+      {data.map((project) => (
+        <article className="my-10" key={project.slug}>
+          {/* heading */}
+          <section className="bg-brand mt-10 inline-flex min-h-[120px] w-full items-center justify-center overflow-hidden rounded-3xl">
+            <h3 className="title text-brand-dark text-pretty uppercase">
+              <TextAnimate>{project.title}</TextAnimate>
+            </h3>
+          </section>
 
-            {/* title with button links */}
-            <section className="mt-10 flex w-full flex-col flex-wrap items-center justify-center gap-4 md:flex-row md:justify-between">
-              <TextAnimate className="title not-italic" as={"h2"}>
-                {project.title}
-              </TextAnimate>
-
-              <div className="flex gap-x-5">
-                {project.demo_url && (
-                  <Button asChild variant="outline">
-                    <a href={project.demo_url} target="_blank" rel="no-refer">
-                      Demonstração <ArrowUpRightIcon />
-                    </a>
-                  </Button>
-                )}
-                {project.repository && (
-                  <Button asChild variant="outline">
-                    <a href={project.repository} target="_blank">
-                      Repositório <ArrowUpRightIcon />
-                    </a>
-                  </Button>
-                )}
-              </div>
-            </section>
-
-            {/* hightlight tags badge */}
-            <section className="mt-5 inline-flex w-full flex-wrap items-center justify-center gap-4 border-b border-white/10 pb-5 md:items-start md:justify-start">
+          {/* skills and external links */}
+          <section className="mx-auto mt-10 flex flex-col flex-wrap items-center justify-between gap-1 border-b border-white/20 pb-6 md:flex-row">
+            {/* badges */}
+            <div className="mb-6 inline-flex flex-1 flex-wrap items-center justify-center gap-4 md:mb-0 md:justify-start">
               {project.tags.map((tag) => (
                 <motion.div
                   animate={{ backgroundPositionX: "-100%" }}
@@ -80,7 +47,7 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
                     repeat: Infinity,
                     ease: "linear",
                     repeatType: "loop",
-                    duration: 2,
+                    duration: 3,
                   }}
                   className={highlightTags(tag)}
                   key={tag}
@@ -88,17 +55,37 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
                   {tag}
                 </motion.div>
               ))}
-            </section>
+            </div>
 
-            {/* overview */}
+            {/* buttons */}
+            <div className="inline-flex flex-1 items-center justify-end gap-4">
+              {project.demo_url && (
+                <Button asChild variant="outline">
+                  <a href={project.demo_url} target="_blank" rel="no-refer">
+                    Demonstração <ArrowUpRightIcon />
+                  </a>
+                </Button>
+              )}
+              {project.repository && (
+                <Button asChild variant="outline">
+                  <a href={project.repository} target="_blank">
+                    Repositório <ArrowUpRightIcon />
+                  </a>
+                </Button>
+              )}
+            </div>
+          </section>
+
+          {/* overview */}
+          <div className="p-4">
             <p
-              className="description mx-auto mt-10 max-w-[900px] text-justify text-pretty"
+              className="description mx-auto mb-4 text-justify text-pretty"
               dangerouslySetInnerHTML={{ __html: project.description }}
             />
 
             {/* image preview */}
             {project.image_url && (
-              <section className="mx-auto mt-10 max-h-[900px] w-[900px] overflow-hidden rounded-2xl">
+              <section className="mx-auto max-h-[900px] w-full overflow-hidden rounded-3xl">
                 <Image
                   unoptimized
                   loading="lazy"
@@ -110,9 +97,9 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
                 />
               </section>
             )}
-          </React.Fragment>
-        ))}
-      </article>
+          </div>
+        </article>
+      ))}
     </section>
   )
 }
