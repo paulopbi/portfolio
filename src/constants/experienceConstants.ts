11e1081cd35bf5df88e1b3fcf1c53c14ac74eaeb
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-28 08:16:39 -0300

    fix: description text

--- src/constants/experienceConstants.ts
+++ src/constants/experienceConstants.ts
@@ -3,14 +3,14 @@ export const experienceSectionData = [
     title: "suporte técnico de redes",
     subtitle: "brisa telecom",
     description:
-      "Foram resolvidos problemas de conectividade com um atendimento ágil e preciso, auxiliando clientes na configuração de modens, roteadores e telefones IP VOIP.",
+      "Administrei dispositivos de rede usando o protocolo TR-69, facilitando o acesso remoto e reduzindo a necessidade de visitas técnicas. Foram resolvidos problemas de conectividade com um atendimento ágil e preciso, auxiliando clientes na configuração de modens, roteadores e telefones IP VOIP. Adotei uma abordagem didática no suporte, explicando soluções de forma clara para aumentar a satisfação do usuário.",
     year: "Fev de 2024 – Ago de 2024",
   },
   {
     title: "operador de logistica I",
     subtitle: "Americanas S.A",
     description:
-      "Trabalho no setor de picking com o sistema WMS, realizando a separação de produtos de forma rápida e precisa, garantindo que fossem corretamente encaminhados para consolidação e envio às lojas.",
+      "Trabalho no setor de picking com o sistema WMS, realizando a separação de produtos de forma rápida e precisa, garantindo que fossem corretamente encaminhados para consolidação e envio às lojas. Trabalho em conformidade com os procedimentos do sistema WMS (Warehouse Management System), que é um software que ajuda a gerir e controlar as operações de armazéns e centros de distribuição.",
     year: "Fev de 2025 – Atualmente",
   },
 ]
