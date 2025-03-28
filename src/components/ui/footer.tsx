c044b12cdc1edd594201585303ef0174cca13575
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-28 10:00:54 -0300

    fix: when it's mobile, the text is in the center

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -4,9 +4,9 @@ const Footer = () => {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="container mt-16 flex w-full flex-col items-center justify-between gap-4 py-6 md:flex-row">
+    <footer className="container mt-16 flex w-full flex-col items-center justify-between gap-4 py-6 text-center md:flex-row md:text-left">
       <p className="text-sm font-normal">
-        &copy; {currentYear} Paulo Victor Silva. Todos os direitos reservados.
+        &copy; {currentYear} Paulo Victor Silva, Todos os direitos reservados.
       </p>
       <div className="inline-flex gap-2">
         <a href="https://github.com/paulopbi" target="_blank" rel="noreferrer">
