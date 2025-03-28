608281ebb96332fe5cf7988f7ab3610b68fc78f9
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-28 07:29:00 -0300

    fix: change particle wrapper and rename the component to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,28 @@ import Footer from "@/components/ui/footer"
 import NavBar from "@/components/ui/navbar"
 import { Particles } from "@/components/magicui/particles"
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <NavBar />
-      <section className="relative z-10">
-        <Particles
-          className="absolute inset-0 z-0 h-full w-full"
-          quantity={290}
-          ease={120}
-          color="#ccc"
-          refresh
-        />
-        <main>
-          <Hero />
-          <Services />
-          <Skills />
-          <Experience />
-          <Education />
-          <ProjectsText />
-          <Projects />
-        </main>
-      </section>
+      <Particles
+        className="absolute inset-0 z-0 h-full w-full"
+        quantity={290}
+        ease={120}
+        color="#ccc"
+        refresh
+      />
+
+      <main className="relative z-10">
+        <Hero />
+        <Services />
+        <Skills />
+        <Experience />
+        <Education />
+        <ProjectsText />
+        <Projects />
+      </main>
+
       <Footer />
     </>
   )
