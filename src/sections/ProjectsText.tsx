d431c549f90cf01319b54b25adae420c82388b3f
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-28 08:13:28 -0300

    fix: font size

--- src/sections/ProjectsText.tsx
+++ src/sections/ProjectsText.tsx
@@ -4,10 +4,10 @@ import { TextReveal } from "@/components/magicui/text-reveal"
 
 const ProjectsText = () => {
   return (
-    <TextReveal className="font-[Bricolage_Grotesque] text-base font-bold text-pretty md:text-2xl">
+    <TextReveal className="font-[Bricolage_Grotesque] text-lg font-bold text-pretty md:text-3xl">
       Agora que você conhece um pouco da minha trajetória, é hora de ver na
       prática como transformo ideias em resultados. Cada projeto é uma nova
-      oportunidade de aprender e entregar soluções criativas. 🚀
+      oportunidade de aprender e entregar soluções criativas.
     </TextReveal>
   )
 }
