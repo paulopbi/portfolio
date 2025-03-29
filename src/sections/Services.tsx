b1d5d3132272571d3419bde815fc2cbd1886f0e1
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-29 17:51:06 -0300

    refactor: add heading title and card services

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { TextAnimate } from "@/components/magicui/text-animate"
+import CardServices from "@/components/ui/card-services"
+import HeadingTitle from "@/components/ui/heading-title"
 import { servicesSectionData } from "@/constants/servicesConstants"
 
 const Services = () => {
@@ -7,32 +7,15 @@ const Services = () => {
     <section className="relative z-10 container mt-20">
       {/* heading */}
       <div className="mb-8 text-center">
-        <TextAnimate
-          className="subtitle--heading"
-          by="character"
-          as={"h5"}
-          animation="scaleDown"
-        >
-          Serviços
-        </TextAnimate>
-        <p className="description">
+        <HeadingTitle title="Serviços">
           Meus serviços abrangem três áreas essenciais no processo de criação.
-        </p>
+        </HeadingTitle>
       </div>
 
       {/* cards */}
       <div className="container grid grid-cols-1 items-center justify-center gap-6 md:grid-cols-2">
         {servicesSectionData.map((service) => (
-          <section
-            className="bg-foreground border-b-brand mx-auto min-h-[200px] w-full rounded-lg border-b p-5 md:last:col-span-2"
-            key={service.title}
-          >
-            <span>
-              <service.icon size={32} color="var(--color-brand)" />
-            </span>
-            <h4 className="title--services">{service.title}</h4>
-            <span className="description">{service.subtitle}</span>
-          </section>
+          <CardServices key={service.title} services={service} />
         ))}
       </div>
     </section>
