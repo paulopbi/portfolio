a0ebe0ea178b092174462eb331e53ecab19b9aba
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-29 17:50:45 -0300

    refactor: add heading title and accordion

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -1,36 +1,64 @@
-import InformativeAccordion from "@/components/informative-accordion"
-import { TextAnimate } from "@/components/magicui/text-animate"
+import Link from "next/link"
+import HeadingTitle from "@/components/ui/heading-title"
+import { AccordionHeader } from "@radix-ui/react-accordion"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 import { educationalSectionData } from "@/constants/educationConstants"
 
 const Education = () => {
   return (
     <section className="container mt-20 grid grid-cols-1 items-center gap-10 md:mt-50 md:grid-cols-2">
       <div className="text-center">
-        <TextAnimate
-          className="subtitle--heading"
-          by="character"
-          as={"h2"}
-          animation="scaleDown"
-        >
-          Estudos e Especialização
-        </TextAnimate>
-        <p className="description mx-auto max-w-[50ch]">
+        <HeadingTitle title="Estudos">
           Para mim, estudo e trabalho andam juntos. Cada nova aprendizagem é um
           passo rumo à excelência no que faço.
-        </p>
+        </HeadingTitle>
       </div>
 
       <article className="mx-auto w-full">
         {educationalSectionData.map((education) => (
-          <InformativeAccordion
+          <Accordion
+            type="single"
+            collapsible
             key={education.title}
-            title={education.title}
-            subtitle={education.subtitle}
-            description={education.description}
-            year={education.year}
-            href={education.href}
-            linkName={education.linkName}
-          />
+            className="w-full border-b border-white/10"
+          >
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                <AccordionHeader className="flex w-full cursor-pointer flex-col items-start">
+                  <div>
+                    <h5 className="title--caption capitalize">
+                      {education.title}
+                    </h5>
+                  </div>
+                  <div className="mt-1 inline-flex w-full items-center justify-between gap-8">
+                    <h6 className="subtitle--caption capitalize">
+                      {education.subtitle}
+                    </h6>
+                    <p className="text-muted/70 text-xs font-light capitalize">
+                      {education.year}
+                    </p>
+                  </div>
+                </AccordionHeader>
+              </AccordionTrigger>
+              <AccordionContent>
+                <p className="description">{education.description}</p>
+
+                {education.linkName && education.href && (
+                  <Link
+                    href={education.href}
+                    className="text-brand mt-1.5 inline-flex items-center gap-4 text-sm font-bold"
+                  >
+                    {education.linkName}
+                  </Link>
+                )}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         ))}
       </article>
     </section>
