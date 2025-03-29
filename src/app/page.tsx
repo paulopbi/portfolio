7ce270b5da38c41f2358f3139e1b930f244b79ec
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-29 17:35:39 -0300

    feat: remove particles to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,11 @@ import ProjectsText from "@/sections/ProjectsText"
 import Services from "@/sections/Services"
 import Footer from "@/components/ui/footer"
 import NavBar from "@/components/ui/navbar"
-import { Particles } from "@/components/magicui/particles"
 
 export default function HomePage() {
   return (
     <>
       <NavBar />
-      <Particles
-        className="absolute inset-0 z-0 h-full w-full"
-        quantity={300}
-        ease={1200}
-        color="#ccc"
-        refresh
-      />
-
       <main className="relative z-10">
         <Hero />
         <Services />
@@ -30,7 +21,6 @@ export default function HomePage() {
         <ProjectsText />
         <Projects />
       </main>
-
       <Footer />
     </>
   )
