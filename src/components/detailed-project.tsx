4317c30e769ee531481ce4ba8d47e27d4956b98a
Paulo Victor Silva <paulpbi16@gmail.com>
2025-03-31 09:43:36 -0300

    refactor: change layout and DetailedProjectInfo component style

--- src/components/detailed-project.tsx
+++ src/components/detailed-project.tsx
@@ -5,39 +5,38 @@ import Link from "next/link"
 import { motion } from "framer-motion"
 import { Button } from "./ui/button"
 import { ArrowLeft, ArrowUpRightIcon } from "lucide-react"
-import { Badge } from "./ui/badge"
 import { projectInfo } from "@/constants/projectConstants"
 import { TextAnimate } from "./magicui/text-animate"
 import { highlightTags } from "@/lib/utils"
 
 const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
   return (
-    <section className="relative z-10 container">
+    <section className="container">
       {/* navegation on top */}
-      <section className="flex items-center justify-between gap-4">
+      <section>
         <Link
           className="text-muted inline-flex items-center justify-between gap-2 text-base font-light"
           href="/projetos"
         >
-          <ArrowLeft size={18} color="#e5e5e5" /> Projetos
+          <ArrowLeft size={18} color="var(--color-muted)" /> Projetos
         </Link>
-
-        <Badge className="text-muted rounded-full border border-white/20 bg-transparent px-2 py-1">
-          {data.map(({ year }) => year)}
-        </Badge>
       </section>
 
       {data.map((project) => (
         <article className="my-10" key={project.slug}>
           {/* heading */}
-          <section className="bg-brand mt-10 inline-flex min-h-[120px] w-full items-center justify-center overflow-hidden rounded-3xl">
-            <h3 className="title text-brand-dark text-pretty uppercase">
-              <TextAnimate>{project.title}</TextAnimate>
-            </h3>
+          <section className="mt-8 flex items-center justify-center">
+            <TextAnimate
+              className="bg-brand title text-brand-dark inline-block w-[80%] overflow-hidden rounded-3xl px-5 py-8 text-center"
+              animation="scaleDown"
+              duration={1}
+              as={"h2"}
+            >
+              {project.title}
+            </TextAnimate>
           </section>
-
           {/* skills and external links */}
-          <section className="mx-auto mt-10 flex flex-col flex-wrap items-center justify-between gap-1 border-b border-white/20 pb-6 md:flex-row">
+          <section className="mx-auto mt-10 flex max-w-[80%] flex-col flex-wrap items-center justify-between gap-1 border-b border-white/20 pb-6 md:flex-row">
             {/* badges */}
             <div className="mb-6 inline-flex flex-1 flex-wrap items-center justify-center gap-4 md:mb-0 md:justify-start">
               {project.tags.map((tag) => (
@@ -77,15 +76,15 @@ const DetailedProjectInfo = ({ data }: { data: typeof projectInfo }) => {
           </section>
 
           {/* overview */}
-          <div className="mt-6">
+          <div className="mx-auto mt-6 mb-4 max-w-[80%]">
             <p
-              className="description mx-auto mb-4 text-justify text-pretty"
+              className="description text-justify text-pretty"
               dangerouslySetInnerHTML={{ __html: project.description }}
             />
 
             {/* image preview */}
             {project.image_url && (
-              <section className="mx-auto max-h-[900px] w-full overflow-hidden rounded-3xl">
+              <section className="mt-4 overflow-hidden rounded-3xl">
                 <Image
                   unoptimized
                   loading="lazy"
